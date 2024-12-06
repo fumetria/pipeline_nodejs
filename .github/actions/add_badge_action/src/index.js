cbf16d1ef9545e63425da026bd7eb0d0a791ee80
Fumetria <fulin789@gmail.com>
2024-12-07 00:42:24 +0100

    fix 8

--- .github/actions/add_badge_action/src/index.js
+++ .github/actions/add_badge_action/src/index.js
@@ -1,15 +1,20 @@
-import * as core from '@actions/core';
-import { promises as fs } from 'fs';
+const core = require('@actions/core');
+const fs = require('fs').promises;
 
-try {
-    const testResult = core.getInput(test_result);
-    const imgFail = 'https://img.shields.io/badge/test-failure-red';
-    const imgSuccess = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg';
-    const badge = testResult === 'success' ? imgSuccess : imgFail;
-    const textBadge = `RESULTAT DELS ÚLTIMS TESTS \n ![Test result badge](${badge})`;
-    await fs.writeFile('.README.md', textBadge);
-    process.exit(0)
+async function main() {
+    
+    try {
+        const testResult = core.getInput(test_result);
+        const imgFail = 'https://img.shields.io/badge/test-failure-red';
+        const imgSuccess = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg';
+        const badge = testResult === 'success' ? imgSuccess : imgFail;
+        const textBadge = `RESULTAT DELS ÚLTIMS TESTS \n ![Test result badge](${badge})`;
+        await fs.writeFile('.README.md', textBadge);
+        process.exit(0);
+    } catch (error) {
+        core.setFailed(error);
+    }
 
-} catch (error) {
-    core.setFailed(error);
-}
+};
+
+main();
