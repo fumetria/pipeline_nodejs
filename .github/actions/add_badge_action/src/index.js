d1d022573a59c1713f478aa775773ac387fa022c
Fu Jun <fulin.fujun@outlook.es>
2024-12-06 16:43:29 +0100

    Add badge job

---
+++ .github/actions/add_badge_action/src/index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const fs = require('fs').promises;
+
+try {
+    const testResult = core.getInput(test_result);
+    const imgFail = 'https://img.shields.io/badge/test-failure-red';
+    const imgSuccess = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg';
+    const badge = testResult === 'success' ? imgSuccess : imgFail;
+    const textBadge = `RESULTAT DELS ÚLTIMS TESTS \n ${badge}`;
+    await fs.writeFile('.README.md', textBadge);
+    process.exit(0)
+
+} catch (error) {
+    core.setFailed(error);
+}
