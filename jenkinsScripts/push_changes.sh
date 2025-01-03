89fc95aba6afcb4dd68174d9a60077792cbdf813
Fu Jun <fulin.fujun@outlook.es>
2025-01-04 00:59:33 +0100

    Fix in push_changes.sh

--- jenkinsScripts/push_changes.sh
+++ jenkinsScripts/push_changes.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-def encodedPassword = URLEncoder.encode("$2",'UTF-8')
 sh "git config --global user.name $1"
 sh "git config --global user.password $2"
 sh "git add ."
 sh "git commit -m 'Pipeline executada per $3. Motiu: $4'"
-sh "git push https://$1:${encodedPassword}@github.com/$1/pipeline_nodejs.git HEAD:ci_jenkins"
+sh "git push https://$1:$2@github.com/$1/pipeline_nodejs.git HEAD:ci_jenkins"
