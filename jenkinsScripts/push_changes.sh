41da327321cfdd22434ec60b7797fec2d718be51
Fu Jun <fulin.fujun@outlook.es>
2025-01-04 01:01:40 +0100

    Fix in push_changes.sh 2

--- jenkinsScripts/push_changes.sh
+++ jenkinsScripts/push_changes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sh "git config --global user.name $1"
-sh "git config --global user.password $2"
-sh "git add ."
-sh "git commit -m 'Pipeline executada per $3. Motiu: $4'"
-sh "git push https://$1:$2@github.com/$1/pipeline_nodejs.git HEAD:ci_jenkins"
+git config --global user.name $1
+git config --global user.password $2
+git add .
+git commit -m 'Pipeline executada per $3. Motiu: $4'
+git push https://$1:$2@github.com/$1/pipeline_nodejs.git HEAD:ci_jenkins
